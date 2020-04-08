33c96c332ee9105230dc7ec4b5e2b88d3fdaec9f
John Butler <johnmichaelbutler@yahoo.com>
2020-04-08 11:47:04 -0500

    Update app with shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
 
 
 function App() {
@@ -15,7 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {
+        // Checks to make sure that only 4 items are passed to shop page then maps over to return a div with item name
+        items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps}/>
+        ))
+      }
+    </div>
+  </div>
+)
+
+export default CollectionPreview;
